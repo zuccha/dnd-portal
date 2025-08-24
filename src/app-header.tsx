a6cbb9155ac1cebba50ba81cd314c56e9affc834
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-08-24 14:55:12 +0200

    Fetch languages and add language select in header

--- src/app-header.tsx
+++ src/app-header.tsx
@@ -1,7 +1,18 @@
-import { Avatar, HStack, Heading, Menu, Portal } from "@chakra-ui/react";
+import {
+  Avatar,
+  HStack,
+  Heading,
+  Menu,
+  Portal,
+  createListCollection,
+} from "@chakra-ui/react";
+import { useMemo } from "react";
+import { i18nLangStore } from "./i18n/i18n-lang";
 import { type AuthUser, signOut } from "./supabase/auth";
+import { useLanguages } from "./supabase/language";
 import useAuth from "./supabase/use-auth";
 import ThemeButton from "./theme/theme-button";
+import Select from "./ui/select";
 
 //------------------------------------------------------------------------------
 // App Header
@@ -22,9 +33,40 @@ export default function AppHeader() {
 
       <HStack>
         {auth.user && <UserButton user={auth.user} />}
+        <LanguageSelect />
         <ThemeButton />
       </HStack>
     </HStack>
+  );
+}
+
+//------------------------------------------------------------------------------
+// Language Select
+//------------------------------------------------------------------------------
+
+function LanguageSelect() {
+  const [language, setLanguage] = i18nLangStore.use();
+
+  const languages = useLanguages();
+  const languageOptions = useMemo(
+    () =>
+      createListCollection({
+        items: languages.data?.map(({ code }) => ({
+          label: code.toUpperCase(),
+          value: code,
+        })) ?? [{ label: "EN", value: "en" }],
+      }),
+    [languages.data]
+  );
+
+  return (
+    <Select
+      onValueChange={setLanguage}
+      options={languageOptions}
+      size="sm"
+      value={language}
+      w="4em"
+    />
   );
 }
 
@@ -36,7 +78,7 @@ function UserButton({ user }: { user: AuthUser }) {
   return (
     <Menu.Root>
       <Menu.Trigger focusRing="outside" rounded="full">
-        <Avatar.Root size="xs">
+        <Avatar.Root mr={2} size="xs">
           <Avatar.Fallback name={user.name} />
           <Avatar.Image src={user.avatarUrl} />
         </Avatar.Root>
---
+++ src/supabase/language.ts
@@ -0,0 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import z from "zod/v4";
+import supabase from "./supabase";
+
+//------------------------------------------------------------------------------
+// Language
+//------------------------------------------------------------------------------
+
+export const languageSchema = z.object({
+  code: z.string(),
+  label: z.string(),
+});
+
+export type Language = z.infer<typeof languageSchema>;
+
+//------------------------------------------------------------------------------
+// Fetch Languages
+//------------------------------------------------------------------------------
+
+export async function fetchLanguages(): Promise<Language[]> {
+  const { data } = await supabase.from("languages").select();
+  return z.array(languageSchema).parse(data);
+}
+
+//------------------------------------------------------------------------------
+// Use Languages
+//------------------------------------------------------------------------------
+
+export function useLanguages() {
+  return useQuery<Language[]>({
+    queryFn: fetchLanguages,
+    queryKey: ["languages"],
+
+    gcTime: 30 * 24 * 60 * 60 * 1000,
+    staleTime: Infinity,
+  });
+}
