6f3f0d13700e2bf6bbd2dac3aed2b7d607ca3478
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-08-25 12:10:11 +0200

    Chunk vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
+import react from "@vitejs/plugin-react-swc";
+// import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
-import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules/@chakra-ui")) return "chakra-ui";
+          if (id.includes("node_modules/@zip.js/zip.js")) return "zip-js";
+          if (id.includes("node_modules/zod")) return "zod";
+          return null;
+        },
+      },
+      plugins: [
+        // visualizer({
+        //   brotliSize: true,
+        //   gzipSize: true,
+        //   open: true,
+        // }),
+      ],
+    },
+  },
   plugins: [react(), svgr()],
 });
