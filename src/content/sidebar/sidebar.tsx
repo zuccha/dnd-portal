38704bd6b3b90672ca64e3455d87d4b076263e76
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-08-25 10:32:51 +0200

    Fetch user campaigns

--- src/content/sidebar/sidebar.tsx
+++ src/content/sidebar/sidebar.tsx
@@ -1,4 +1,10 @@
 import { VStack, createListCollection } from "@chakra-ui/react";
+import { useLayoutEffect, useMemo } from "react";
+import { useI18n } from "../../i18n/i18n";
+import {
+  useSelectedUserCampaign,
+  useUserCampaigns,
+} from "../../supabase/resources/campaigns";
 import Select from "../../ui/select";
 
 //------------------------------------------------------------------------------
@@ -6,14 +12,53 @@ import Select from "../../ui/select";
 //------------------------------------------------------------------------------
 
 export default function Sidebar() {
+  const [selectedCampaignId, setSelectedCampaignId] = useSelectedUserCampaign();
+  const { isPending, data: campaigns } = useUserCampaigns();
+
+  const i18n = useI18n(i18nContext);
+
+  const campaignOptions = useMemo(() => {
+    if (isPending)
+      return createListCollection({
+        items: [{ label: "", value: "" }],
+      });
+    if (!campaigns || !campaigns.length)
+      return createListCollection({
+        items: [{ label: i18n.t("select.empty"), value: "" }],
+      });
+    return createListCollection({
+      items: campaigns.map((campaign) => ({
+        label: campaign.name,
+        value: campaign.id,
+      })),
+    });
+  }, [campaigns, i18n, isPending]);
+
+  useLayoutEffect(() => {
+    setSelectedCampaignId(
+      campaigns && campaigns.length ? campaigns[0].id : undefined
+    );
+  }, [campaigns, setSelectedCampaignId]);
+
   return (
     <VStack borderRightWidth={1} flex={1} h="full" maxW="20em" p={4}>
-      <Select onValueChange={() => {}} options={campaigns} value="arvenna" />
+      <Select
+        disabled={isPending || !campaigns || !campaigns.length}
+        onValueChange={setSelectedCampaignId}
+        options={campaignOptions}
+        value={selectedCampaignId ?? ""}
+      />
     </VStack>
   );
 }
 
-// TODO: Replace with actual data.
-const campaigns = createListCollection({
-  items: [{ label: "Arvenna", value: "arvenna" }],
-});
+//------------------------------------------------------------------------------
+// I18n Context
+//------------------------------------------------------------------------------
+
+const i18nContext = {
+  "select.empty": {
+    en: "There are no campaigns",
+    it: "Non ci sono campagne",
+  },
+};
---
+++ src/supabase/resources/campaigns.ts
@@ -0,0 +1,47 @@
+import { useQuery } from "@tanstack/react-query";
+import { z } from "zod/v4";
+import { createMemoryStore } from "../../store/memory-store";
+import supabase from "../supabase";
+
+//------------------------------------------------------------------------------
+// Campaign
+//------------------------------------------------------------------------------
+
+export const campaignSchema = z.object({
+  core: z.boolean(),
+  id: z.string(),
+  name: z.string(),
+});
+
+export type Campaign = z.infer<typeof campaignSchema>;
+
+//------------------------------------------------------------------------------
+// Fetch User Campaigns
+//------------------------------------------------------------------------------
+
+export async function fetchUserCampaigns(): Promise<Campaign[]> {
+  const { data } = await supabase.from("campaigns").select().eq("core", false);
+  return z.array(campaignSchema).parse(data);
+}
+
+//------------------------------------------------------------------------------
+// Use User Campaigns
+//------------------------------------------------------------------------------
+
+export function useUserCampaigns() {
+  return useQuery<Campaign[]>({
+    queryFn: fetchUserCampaigns,
+    queryKey: ["campaigns"],
+
+    gcTime: 30 * 24 * 60 * 60 * 1000,
+    staleTime: Infinity,
+  });
+}
+
+//------------------------------------------------------------------------------
+// Use Selected User Campaign
+//------------------------------------------------------------------------------
+
+export const { use: useSelectedUserCampaign } = createMemoryStore<
+  string | undefined
+>(undefined);
