f38206ce24db2df831bc657150e96767a554e774
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-08-26 16:28:37 +0200

    Show ellipsis and abbr text for text that exceeds column width

--- src/ui/data-table.tsx
+++ src/ui/data-table.tsx
@@ -1,4 +1,5 @@
 import { Span, Table, type TableRootProps } from "@chakra-ui/react";
+import { useEffect, useRef, useState } from "react";
 import Checkbox from "./checkbox";
 
 //------------------------------------------------------------------------------
@@ -60,19 +61,15 @@ export default function DataTable<T extends { id: string }>({
             <Table.Row key={row.id}>
               {columns.map(({ key, ...rest }) => {
                 const value = row[key];
+                if (typeof value === "string")
+                  return <TableCellString key={key} value={value} {...rest} />;
+
+                if (typeof value === "boolean")
+                  return <TableCellBoolean key={key} value={value} {...rest} />;
+
                 return (
-                  <Table.Cell
-                    key={String(key)}
-                    overflow="hidden"
-                    textOverflow="ellipsis"
-                    whiteSpace="nowrap"
-                    {...rest}
-                  >
-                    {typeof value === "boolean" ? (
-                      <Checkbox checked={value} disabled size="sm" />
-                    ) : (
-                      String(value)
-                    )}
+                  <Table.Cell key={String(key)} {...rest}>
+                    {String(value)}
                   </Table.Cell>
                 );
               })}
@@ -81,5 +78,49 @@ export default function DataTable<T extends { id: string }>({
         })}
       </Table.Body>
     </Table.Root>
+  );
+}
+
+//------------------------------------------------------------------------------
+// Table Cell String
+//------------------------------------------------------------------------------
+
+function TableCellString({
+  value,
+  ...rest
+}: Omit<Table.CellProps, "children"> & { value: string }) {
+  const ref = useRef<HTMLTableCellElement>(null);
+  const [overflow, setOverflow] = useState(false);
+
+  useEffect(() => {
+    const tableCell = ref.current;
+    if (tableCell) setOverflow(tableCell.scrollWidth > tableCell.clientWidth);
+  }, [value]);
+
+  return (
+    <Table.Cell
+      overflow="hidden"
+      ref={ref}
+      textOverflow="ellipsis"
+      whiteSpace="nowrap"
+      {...rest}
+    >
+      {overflow ? <abbr title={value}>{value}</abbr> : value}
+    </Table.Cell>
+  );
+}
+
+//------------------------------------------------------------------------------
+//  Table Cell Boolean
+//------------------------------------------------------------------------------
+
+function TableCellBoolean({
+  value,
+  ...rest
+}: Omit<Table.CellProps, "children"> & { value: boolean }) {
+  return (
+    <Table.Cell {...rest}>
+      <Checkbox checked={value} disabled size="sm" />
+    </Table.Cell>
   );
 }
