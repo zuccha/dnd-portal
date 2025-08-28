6e520924ee96a82fc864089bfb11b8a4707770c0
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-08-28 21:17:57 +0200

    Add switch to spells panel

---
+++ src/app/body/screens/campaign-screen/panels/spells-panel/spells-list-cards.tsx
@@ -0,0 +1,11 @@
+//------------------------------------------------------------------------------
+// Spells List Cards
+//------------------------------------------------------------------------------
+
+export type SpellsListCardsProps = {
+  campaignId: string;
+};
+
+export default function SpellsListCards(_props: SpellsListCardsProps) {
+  return null;
+}
--- src/app/body/screens/campaign-screen/panels/spells-panel/spells-panel.tsx
+++ src/app/body/screens/campaign-screen/panels/spells-panel/spells-panel.tsx
@@ -1,5 +1,12 @@
 import { Flex, HStack, VStack } from "@chakra-ui/react";
+import { Grid2X2Icon, ListIcon } from "lucide-react";
+import z from "zod/v4";
+import { createLocalStore } from "../../../../../../store/local-store";
+import BinaryButton, {
+  type BinaryButtonProps,
+} from "../../../../../../ui/binary-button";
 import SpellsFilters from "./spells-filters";
+import SpellsListCards from "./spells-list-cards";
 import SpellsListTable from "./spells-list-table";
 
 //------------------------------------------------------------------------------
@@ -11,22 +18,47 @@ export type SpellsPanelProps = {
 };
 
 export default function SpellsPanel({ campaignId }: SpellsPanelProps) {
+  const [view, setView] = useView();
+
   return (
     <VStack flex={1} gap={0} h="full" overflow="auto" w="full">
       <HStack
         borderBottomWidth={1}
         h="full"
+        justify="space-between"
         maxH="4em"
         overflow="auto"
         p={2}
         w="full"
       >
         <SpellsFilters />
+
+        <BinaryButton
+          onValueChange={setView}
+          options={viewOptions}
+          value={view}
+        />
       </HStack>
 
       <Flex maxH="full" overflow="auto" w="full">
-        <SpellsListTable campaignId={campaignId} />
+        {view === "table" && <SpellsListTable campaignId={campaignId} />}
+        {view === "cards" && <SpellsListCards campaignId={campaignId} />}
       </Flex>
     </VStack>
   );
 }
+
+//------------------------------------------------------------------------------
+// View
+//------------------------------------------------------------------------------
+
+const useView = createLocalStore(
+  "resources.spells.view",
+  "table",
+  z.enum(["cards", "table"]).parse
+).use;
+
+const viewOptions: BinaryButtonProps<"table", "cards">["options"] = [
+  { Icon: ListIcon, value: "table" },
+  { Icon: Grid2X2Icon, value: "cards" },
+];
