36a57402a2312648d7e49e0cbd5628d275eace2a
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-08-31 14:26:57 +0200

    Add function to download JSON files

---
+++ src/utils/download.ts
@@ -0,0 +1,11 @@
+export function downloadJson(json: string, filename: string) {
+  const blob = new Blob([json], { type: "application/json;charset=utf-8" });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  a.remove();
+  URL.revokeObjectURL(url);
+}
