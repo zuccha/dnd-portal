01c01dcc0596eb3ac719867c01b1a9b4ab662357
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-09-01 17:36:05 +0200

    Create field component

---
+++ src/ui/field.tsx
@@ -0,0 +1,40 @@
+import {
+  Field as ChakraField,
+  type FieldRootProps as ChakraFieldRootProps,
+} from "@chakra-ui/react";
+
+//------------------------------------------------------------------------------
+// Field
+//------------------------------------------------------------------------------
+
+export type FieldProps = ChakraFieldRootProps & {
+  label: string;
+  errorText?: string;
+  helperText?: string;
+  optionalText?: string;
+};
+
+export default function Field({
+  children,
+  errorText,
+  helperText,
+  optionalText,
+  label,
+  ...rest
+}: FieldProps) {
+  return (
+    <ChakraField.Root {...rest}>
+      {label && (
+        <ChakraField.Label>
+          {label}
+          <ChakraField.RequiredIndicator fallback={optionalText} />
+        </ChakraField.Label>
+      )}
+      {children}
+      {helperText && (
+        <ChakraField.HelperText>{helperText}</ChakraField.HelperText>
+      )}
+      {errorText && <ChakraField.ErrorText>{errorText}</ChakraField.ErrorText>}
+    </ChakraField.Root>
+  );
+}
