21c9fea0f89b8d8e94d6d006b942ff467ab9f28c
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-09-01 17:34:37 +0200

    Export function to parse measure

--- src/i18n/i18n-measure.ts
+++ src/i18n/i18n-measure.ts
@@ -13,6 +13,23 @@ export function computeMeasureRegex(units: string[]) {
 }
 
 //------------------------------------------------------------------------------
+// Parse Measure
+//------------------------------------------------------------------------------
+
+export function parseMeasure(
+  measure: string,
+  units: string[]
+): [number, string] | undefined {
+  const regex = computeMeasureRegex(units);
+  const match = measure.match(regex);
+  if (!match || !match.groups) return undefined;
+
+  const unit = match.groups.unit;
+  const value = parseFloat(match.groups.value) || 0;
+  return [value, unit];
+}
+
+//------------------------------------------------------------------------------
 // Use Translate Measure
 //------------------------------------------------------------------------------
 
@@ -24,13 +41,11 @@ export function useTranslateMeasure(
   const { tpi } = useI18nLangContext(context);
 
   return useCallback(
-    (raw: string) => {
-      const regex = computeMeasureRegex(units);
-      const match = raw.match(regex);
-      if (!match || !match.groups) return raw;
+    (measure: string) => {
+      const result = parseMeasure(measure, units) ?? [0, ""];
+      if (!result) return measure;
 
-      const unit = match.groups.unit;
-      const value = parseFloat(match.groups.value);
+      const [value, unit] = result;
       return tpi(`${unit}.${format}`, value, `${value}`);
     },
     [format, tpi, units]
