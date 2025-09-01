490d2f12fdd686b79646b430153f5cfad66d4055
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-09-01 18:08:55 +0200

    Export functions to parse distance and distance unit options

--- src/i18n/i18n-distance.ts
+++ src/i18n/i18n-distance.ts
@@ -1,5 +1,11 @@
 import z from "zod";
-import { computeMeasureRegex, useTranslateMeasure } from "./i18n-measure";
+import {
+  computeMeasureRegex,
+  parseMeasure,
+  useTranslateMeasure,
+} from "./i18n-measure";
+import { useI18nLangContext } from "./i18n-lang-context";
+import { useMemo } from "react";
 
 //------------------------------------------------------------------------------
 // Distance
@@ -16,6 +22,22 @@ export const distanceImpSchema = z.string().regex(distanceImpRegex);
 export const distanceMetSchema = z.string().regex(distanceMetRegex);
 
 //------------------------------------------------------------------------------
+// Parse Distance Imp
+//------------------------------------------------------------------------------
+
+export function parseDistanceImp(distance: string) {
+  return parseMeasure(distance, distanceImpUnits);
+}
+
+//------------------------------------------------------------------------------
+// Parse Distance Met
+//------------------------------------------------------------------------------
+
+export function parseDistanceMet(distance: string) {
+  return parseMeasure(distance, distanceMetUnits);
+}
+
+//------------------------------------------------------------------------------
 // Use Translate Distance
 //------------------------------------------------------------------------------
 
@@ -26,10 +48,50 @@ export function useTranslateDistance(
 }
 
 //------------------------------------------------------------------------------
+// Use Distance Imp Unit Options
+//------------------------------------------------------------------------------
+
+export function useDistanceImpUnitOptions() {
+  const { t } = useI18nLangContext(i18Context);
+
+  return useMemo(
+    () =>
+      distanceImpUnits.map((unit) => ({
+        label: t(`${unit}.unit`),
+        value: unit,
+      })),
+    [t]
+  );
+}
+
+//------------------------------------------------------------------------------
+// Use Distance Met Unit Options
+//------------------------------------------------------------------------------
+
+export function useDistanceMetUnitOptions() {
+  const { t } = useI18nLangContext(i18Context);
+
+  return useMemo(
+    () =>
+      distanceMetUnits.map((unit) => ({
+        label: t(`${unit}.unit`),
+        value: unit,
+      })),
+    [t]
+  );
+}
+
+//------------------------------------------------------------------------------
 // I18n Context
 //------------------------------------------------------------------------------
 
 const i18Context = {
+  "cm.unit": { en: "Centimeters", it: "Centimetri" },
+  "ft.unit": { en: "Feet", it: "Piedi" },
+  "km.unit": { en: "Kilometers", it: "Chilometri" },
+  "m.unit": { en: "Meters", it: "Metri" },
+  "mi.unit": { en: "Miles", it: "Miglia" },
+
   "cm.long/*": { en: "<1> centimeters", it: "<1> centimetri" },
   "cm.long/1": { en: "<1> centimeters", it: "<1> centimetro" },
   "ft.long/*": { en: "<1> feet", it: "<1> piedi" },
