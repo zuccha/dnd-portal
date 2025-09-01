ad8ff60e816fa2f6029a51485dbc33ea8d7580e9
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-09-01 08:42:32 +0200

    Add hook to fetch user role in campaign

---
+++ src/resources/campaign-role.ts
@@ -0,0 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import z from "zod";
+import supabase from "../supabase";
+
+//------------------------------------------------------------------------------
+// Campaign Role
+//------------------------------------------------------------------------------
+
+export const campaignRoleSchema = z.enum(["game_master", "player"]);
+
+export type CampaignRole = z.infer<typeof campaignRoleSchema>;
+
+//------------------------------------------------------------------------------
+// Fetch Campaign Role
+//------------------------------------------------------------------------------
+
+export async function fetchCampaignRole(
+  campaignId: string
+): Promise<CampaignRole> {
+  const { data } = await supabase.rpc("fetch_campaign_role", {
+    p_campaign_id: campaignId,
+  });
+  return campaignRoleSchema.parse(data);
+}
+
+//------------------------------------------------------------------------------
+// Use Campaign Role
+//------------------------------------------------------------------------------
+
+export function useCampaignRole(campaignId: string) {
+  return useQuery<CampaignRole>({
+    queryFn: () => fetchCampaignRole(campaignId),
+    queryKey: ["campaign-role", campaignId],
+  });
+}
