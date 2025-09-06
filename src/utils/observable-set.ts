a4d18b18690b777671a294ca0681b0a1a9a3165f
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-09-06 15:28:14 +0200

    Allow different kind of keys in observable set

--- src/utils/observable-set.ts
+++ src/utils/observable-set.ts
@@ -5,10 +5,10 @@ import { createObservable } from "./observable";
 // Observable Set
 //------------------------------------------------------------------------------
 
-export type ObservableSet<T> = {
-  notify: (id: string, value: T) => void;
-  subscribe: (id: string, callback: Callback1<T>) => () => void;
-  unsubscribe: (id: string, callback: Callback1<T>) => void;
+export type ObservableSet<T, K extends PropertyKey = string> = {
+  notify: (id: K, value: T) => void;
+  subscribe: (id: K, callback: Callback1<T>) => () => void;
+  unsubscribe: (id: K, callback: Callback1<T>) => void;
 
   notifyAll: (value: T) => void;
   subscribeAll: (callback: Callback1<T>) => () => void;
@@ -19,21 +19,24 @@ export type ObservableSet<T> = {
 // Create Observable Set
 //------------------------------------------------------------------------------
 
-export function createObservableSet<T>(): ObservableSet<T> {
+export function createObservableSet<
+  T,
+  K extends PropertyKey = string
+>(): ObservableSet<T, K> {
   const {
     notify: notifyAll,
     subscribe: subscribeAll,
     unsubscribe: unsubscribeAll,
   } = createObservable<T>();
 
-  const listeners = new Map<string, Set<Callback1<T>>>();
+  const listeners = new Map<K, Set<Callback1<T>>>();
 
-  function notify(id: string, value: T): void {
+  function notify(id: K, value: T): void {
     listeners.get(id)?.forEach((callback) => callback(value));
     notifyAll(value);
   }
 
-  function subscribe(id: string, callback: Callback1<T>): () => void {
+  function subscribe(id: K, callback: Callback1<T>): () => void {
     if (!listeners.has(id)) listeners.set(id, new Set());
     listeners.get(id)!.add(callback);
 
@@ -43,7 +46,7 @@ export function createObservableSet<T>(): ObservableSet<T> {
     };
   }
 
-  function unsubscribe(id: string, callback: Callback1<T>): void {
+  function unsubscribe(id: K, callback: Callback1<T>): void {
     listeners.get(id)?.delete(callback);
     if (listeners.get(id)?.size === 0) listeners.delete(id);
   }
