bb159cc6ac22102fb0b704039b04585e08e03f9c
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-09-06 13:48:14 +0200

    Add methods to subscribe to all notifications in observable set

--- src/utils/observable-set.ts
+++ src/utils/observable-set.ts
@@ -1,4 +1,5 @@
 import type { Callback1 } from "./callback";
+import { createObservable } from "./observable";
 
 //------------------------------------------------------------------------------
 // Observable Set
@@ -8,6 +9,10 @@ export type ObservableSet<T> = {
   notify: (id: string, value: T) => void;
   subscribe: (id: string, callback: Callback1<T>) => () => void;
   unsubscribe: (id: string, callback: Callback1<T>) => void;
+
+  notifyAll: (value: T) => void;
+  subscribeAll: (callback: Callback1<T>) => () => void;
+  unsubscribeAll: (callback: Callback1<T>) => void;
 };
 
 //------------------------------------------------------------------------------
@@ -15,10 +20,17 @@ export type ObservableSet<T> = {
 //------------------------------------------------------------------------------
 
 export function createObservableSet<T>(): ObservableSet<T> {
+  const {
+    notify: notifyAll,
+    subscribe: subscribeAll,
+    unsubscribe: unsubscribeAll,
+  } = createObservable<T>();
+
   const listeners = new Map<string, Set<Callback1<T>>>();
 
   function notify(id: string, value: T): void {
     listeners.get(id)?.forEach((callback) => callback(value));
+    notifyAll(value);
   }
 
   function subscribe(id: string, callback: Callback1<T>): () => void {
@@ -36,5 +48,13 @@ export function createObservableSet<T>(): ObservableSet<T> {
     if (listeners.get(id)?.size === 0) listeners.delete(id);
   }
 
-  return { notify, subscribe, unsubscribe };
+  return {
+    notify,
+    subscribe,
+    unsubscribe,
+
+    notifyAll,
+    subscribeAll,
+    unsubscribeAll,
+  };
 }
