54539de0c6513726afab06484026d11c1e94f0e2
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-09-06 21:22:57 +0200

    Create memory set store

--- src/store/store.ts
+++ src/store/store.ts
@@ -13,6 +13,16 @@ export type StoreAction<T> = (prevValue: T) => T;
 export type StoreUpdater<T> = (valueOrAction: T | StoreAction<T>) => T;
 
 //------------------------------------------------------------------------------
+// Set Store Updater
+//------------------------------------------------------------------------------
+
+export type SetStoreUpdater<T, K extends PropertyKey = string> = (
+  id: K,
+  valueOrAction: T | StoreAction<T>,
+  defaultValue: T
+) => T;
+
+//------------------------------------------------------------------------------
 // Store
 //------------------------------------------------------------------------------
 
@@ -27,6 +37,30 @@ export type Store<T> = {
   subscribe: (callback: Callback1<T>) => void;
   unsubscribe: (callback: Callback1<T>) => void;
 };
+
+//------------------------------------------------------------------------------
+// Set Store
+//------------------------------------------------------------------------------
+
+export type SetStore<T, K extends PropertyKey = string> = {
+  keys: () => K[];
+
+  get: (id: K, defaultValue: T) => T;
+  set: SetStoreUpdater<T, K>;
+
+  use: (id: K, defaultValue: T) => [T, StoreUpdater<T>];
+  useSetValue: (id: K, defaultValue: T) => StoreUpdater<T>;
+  useValue: (id: K, defaultValue: T) => T;
+
+  subscribe: (id: K, callback: Callback1<T>) => void;
+  subscribeAll: (callback: Callback1<T>) => void;
+  unsubscribe: (id: K, callback: Callback1<T>) => void;
+  unsubscribeAll: (callback: Callback1<T>) => void;
+};
+
+//------------------------------------------------------------------------------
+// Is Store Action
+//------------------------------------------------------------------------------
 
 export function isStoreAction<T>(
   valueOrAction: T | StoreAction<T>
