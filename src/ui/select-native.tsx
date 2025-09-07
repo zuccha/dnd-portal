d73b70b3fb0fc743641e07bf75606ca0bf783ea0
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-09-07 16:38:16 +0200

    Create native select component

---
+++ src/ui/select-native.tsx
@@ -0,0 +1,49 @@
+import {
+  NativeSelect as ChakraNativeSelect,
+  type NativeSelectRootProps as ChakraNativeSelectRootProps,
+} from "@chakra-ui/react";
+
+//------------------------------------------------------------------------------
+// Select Native
+//------------------------------------------------------------------------------
+
+export type SelectNativeOption<T> = { label: string; value: T };
+
+export type SelectNativeProps<T extends string> = Omit<
+  ChakraNativeSelectRootProps,
+  "defaultValue" | "onValueChange" | "value"
+> & {
+  options: SelectNativeOption<T>[];
+  defaultValue?: T;
+  onValueChange?: (value: T) => void;
+  value?: T;
+};
+
+export default function Select<T extends string>({
+  onValueChange,
+  options,
+  value,
+  ...rest
+}: SelectNativeProps<T>) {
+  return (
+    <ChakraNativeSelect.Root {...rest}>
+      <ChakraNativeSelect.Field
+        onChange={
+          onValueChange
+            ? (e) => onValueChange(e.currentTarget.value as T)
+            : undefined
+        }
+        rounded={0}
+        value={value}
+      >
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </ChakraNativeSelect.Field>
+
+      <ChakraNativeSelect.Indicator />
+    </ChakraNativeSelect.Root>
+  );
+}
