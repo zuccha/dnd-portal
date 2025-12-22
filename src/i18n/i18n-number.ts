836dbfdbb97c55c76a6191307a9cdd7e25e10895
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-12-22 10:09:39 +0100

    feat(ui): create i18n number

---
+++ src/i18n/i18n-number.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+import type { I18nLang } from "./i18n-lang";
+
+//------------------------------------------------------------------------------
+// I18n Number
+//------------------------------------------------------------------------------
+
+export const i18nNumberSchema = z.record(z.string(), z.number().nullish());
+
+export type I18nNumber = z.infer<typeof i18nNumberSchema>;
+
+//------------------------------------------------------------------------------
+// Translate
+//------------------------------------------------------------------------------
+
+export function translateNumber(
+  i18nNumber: I18nNumber,
+  lang: I18nLang,
+  fallback: number = 0,
+): number {
+  return i18nNumber[lang] ?? i18nNumber["en"] ?? fallback;
+}
