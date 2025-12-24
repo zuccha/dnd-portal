d1ac606abea7b6e73bc70cfda8ff7782d2386ba2
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2025-12-24 14:49:07 +0100

    feat(ui): create format signed function

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,6 +1,18 @@
+//------------------------------------------------------------------------------
+// Format Number
+//------------------------------------------------------------------------------
+
 const fmt = new Intl.NumberFormat("en-US", {
   maximumFractionDigits: 3,
   minimumFractionDigits: 0,
 });
 
 export const formatNumber = fmt.format;
+
+//------------------------------------------------------------------------------
+// Format Signed
+//------------------------------------------------------------------------------
+
+export function formatSigned(n: number): string {
+  return n < 0 ? `${n}` : `+${n}`;
+}
