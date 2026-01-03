4aa61fa4a270753f1653f75162217985081faebb
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2026-01-03 10:23:23 +0100

    feat(db): add function to fetch resources as options

--- supabase/schemas/04_resources.sql
+++ supabase/schemas/04_resources.sql
@@ -385,6 +385,47 @@ GRANT ALL ON FUNCTION public.fetch_resources(p_campaign_id uuid, p_langs text[],
 
 
 --------------------------------------------------------------------------------
+-- FETCH RESOURCE OPTIONS
+--------------------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION public.fetch_resource_options(
+  p_campaign_id uuid,
+  p_resource_kind public.resource_kind)
+RETURNS TABLE(id uuid, name jsonb)
+LANGUAGE sql
+SET search_path TO 'public', 'pg_temp'
+AS $$
+  WITH campaign_ids AS (
+    SELECT p_campaign_id AS id
+    UNION
+    SELECT cm.module_id
+    FROM public.campaign_modules cm
+    WHERE cm.campaign_id = p_campaign_id
+  )
+  SELECT
+    r.id,
+    coalesce(tt.name, '{}'::jsonb) AS name
+  FROM public.resources r
+  JOIN campaign_ids cids ON cids.id = r.campaign_id
+  LEFT JOIN (
+    SELECT
+      rt.resource_id AS id,
+      jsonb_object_agg(rt.lang, rt.name) AS name
+    FROM public.resource_translations rt
+    GROUP BY rt.resource_id
+  ) tt ON tt.id = r.id
+  WHERE r.kind = p_resource_kind
+  ORDER BY r.id;
+$$;
+
+ALTER FUNCTION public.fetch_resource_options(p_campaign_id uuid, p_resource_kind public.resource_kind) OWNER TO postgres;
+
+GRANT ALL ON FUNCTION public.fetch_resource_options(p_campaign_id uuid, p_resource_kind public.resource_kind) TO anon;
+GRANT ALL ON FUNCTION public.fetch_resource_options(p_campaign_id uuid, p_resource_kind public.resource_kind) TO authenticated;
+GRANT ALL ON FUNCTION public.fetch_resource_options(p_campaign_id uuid, p_resource_kind public.resource_kind) TO service_role;
+
+
+--------------------------------------------------------------------------------
 -- UPSERT RESOURCE TRANSLATION
 --------------------------------------------------------------------------------
 
