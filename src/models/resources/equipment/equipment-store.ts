dab3563535da5a39be5896757bdd28134369220d
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2026-01-04 22:42:35 +0100

    feat(ui): create equipment store

--- src/models/resources/equipment/equipment-store.ts
+++ src/models/resources/equipment/equipment-store.ts
@@ -1,12 +1,25 @@
-import type { ZodType } from "zod";
-import type { I18nString } from "~/i18n/i18n-string";
-import type { TranslationFields } from "../resource";
+import { type ZodType } from "zod";
+import { type I18nString } from "~/i18n/i18n-string";
 import type { ResourceKind } from "../../types/resource-kind";
+import { type TranslationFields } from "../resource";
 import { type ResourceStore, createResourceStore } from "../resource-store";
 import type { DBEquipment, DBEquipmentTranslation } from "./db-equipment";
-import { type Equipment } from "./equipment";
-import type { EquipmentFilters } from "./equipment-filters";
-import { type LocalizedEquipment } from "./localized-equipment";
+import {
+  type Equipment,
+  defaultEquipment,
+  equipmentSchema,
+  equipmentTranslationFields,
+} from "./equipment";
+import {
+  type EquipmentFilters,
+  defaultEquipmentFilters,
+  equipmentFiltersSchema,
+  equipmentOrderOptions,
+} from "./equipment-filters";
+import {
+  type LocalizedEquipment,
+  useLocalizeEquipment,
+} from "./localized-equipment";
 
 //------------------------------------------------------------------------------
 // Create Equipment Store
@@ -42,3 +55,21 @@ export function createEquipmentStore<
     useLocalizeResource: extra.useLocalizeEquipment,
   });
 }
+
+//------------------------------------------------------------------------------
+// Equipment Store
+//------------------------------------------------------------------------------
+
+export const equipmentStore = createResourceStore(
+  { p: "equipments", s: "equipment" },
+  {
+    defaultFilters: defaultEquipmentFilters,
+    defaultResource: defaultEquipment,
+    filtersSchema: equipmentFiltersSchema,
+    kinds: ["armor", "item", "tool", "weapon"],
+    orderOptions: equipmentOrderOptions,
+    resourceSchema: equipmentSchema,
+    translationFields: equipmentTranslationFields,
+    useLocalizeResource: useLocalizeEquipment,
+  },
+);
