17fad1d5b2204935b44d48ba12c140ee034d4b79
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2026-01-11 17:33:39 +0100

    fix(ui): refresh localize resource name function when lookups are done fetching

--- src/models/resources/resource-store.ts
+++ src/models/resources/resource-store.ts
@@ -536,7 +536,7 @@ export function createResourceStore<
     campaignId: string,
     lang: string,
   ): (resourceId: string) => string {
-    useResourceLookupIds(campaignId);
+    const lookupIds = useResourceLookupIds(campaignId);
 
     return useCallback(
       (resourceId: string) => {
@@ -544,7 +544,7 @@ export function createResourceStore<
           resourceLookupCache.get(resourceId) ?? defaultResourceLookup;
         return translate(lookup.name, lang);
       },
-      [lang],
+      [lang, lookupIds], // eslint-disable-line react-hooks/exhaustive-deps
     );
   }
 
