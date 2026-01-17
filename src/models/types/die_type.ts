c93389cbb4c8bf9e40dd33c579b7b626363957b8
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2026-01-17 11:19:10 +0100

    feat(ui): remove d2 and d100 die types

--- src/models/types/die_type.ts
+++ src/models/types/die_type.ts
@@ -5,16 +5,7 @@ import { createTypeTranslationHooks } from "./_base";
 // Die Type
 //------------------------------------------------------------------------------
 
-export const dieTypeSchema = z.enum([
-  "d2",
-  "d4",
-  "d6",
-  "d8",
-  "d10",
-  "d12",
-  "d20",
-  "d100",
-]);
+export const dieTypeSchema = z.enum(["d4", "d6", "d8", "d10", "d12", "d20"]);
 
 export const dieTypes = dieTypeSchema.options;
 
@@ -29,12 +20,10 @@ export const {
   useTranslate: useTranslateDieType,
   useTranslations: useDieTypeTranslations,
 } = createTypeTranslationHooks(dieTypes, {
-  d2: { en: "d2", it: "d2" },
   d4: { en: "d4", it: "d4" },
   d6: { en: "d6", it: "d6" },
   d8: { en: "d8", it: "d8" },
   d10: { en: "d10", it: "d10" },
   d12: { en: "d12", it: "d12" },
   d20: { en: "d20", it: "d20" },
-  d100: { en: "d100", it: "d100" },
 });
