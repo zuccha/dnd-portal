3ae491982e8c76449eee6016ffa124e4b978a1ba
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2026-01-18 23:01:19 +0100

    feat(ui): add musical instrument and gaming set tool types

--- src/models/types/tool-type.ts
+++ src/models/types/tool-type.ts
@@ -5,7 +5,12 @@ import { createTypeTranslationHooks } from "./_base";
 // Tool Type
 //------------------------------------------------------------------------------
 
-export const toolTypeSchema = z.enum(["artisan", "other"]);
+export const toolTypeSchema = z.enum([
+  "artisan",
+  "gaming_set",
+  "musical_instrument",
+  "other",
+]);
 
 export const toolTypes = toolTypeSchema.options;
 
@@ -21,5 +26,7 @@ export const {
   useTranslations: useToolTypeTranslations,
 } = createTypeTranslationHooks(toolTypes, {
   artisan: { en: "Artisan's Tool", it: "Strumento da Artigiano" },
+  gaming_set: { en: "Gaming Set", it: "Gioco" },
+  musical_instrument: { en: "Musical Instrument", it: "Strumento Musicale" },
   other: { en: "Other Tools", it: "Altri Strumenti" },
 });
