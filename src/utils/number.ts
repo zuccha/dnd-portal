0ba805a085d3ce5efc197a7acf51075b70ae1a6b
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2026-01-19 20:04:43 +0100

    feat(ui): add function to format numbers as words

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -23,6 +23,45 @@ export function formatNumber(value: number, lang: string): string {
 }
 
 //------------------------------------------------------------------------------
+// Format Number As Word
+//------------------------------------------------------------------------------
+
+const numberAsWord: Record<string, Record<number, string>> = {
+  en: {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+  },
+  it: {
+    0: "zero",
+    1: "uno",
+    2: "due",
+    3: "tre",
+    4: "quattro",
+    5: "cinque",
+    6: "sei",
+    7: "sette",
+    8: "otto",
+    9: "nove",
+    10: "dieci",
+  },
+};
+
+export function formatNumberAsWord(value: number, lang: string): string {
+  const word = numberAsWord[lang]?.[value];
+  if (word) return word;
+  return formatNumber(value, lang);
+}
+
+//------------------------------------------------------------------------------
 // Format Signed
 //------------------------------------------------------------------------------
 
