be42e8865f79093e378a39f5e1288fec2b3676f8
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2026-06-06 15:20:15 +0200

    chore: delete old canvas stuff

--- src/canvas/models/data.ts
+++
@@ -1,9 +0,0 @@
-import z from "zod";
-
-//------------------------------------------------------------------------------
-// Data
-//------------------------------------------------------------------------------
-
-export const dataSchema = z.record(z.string(), z.json());
-
-export type Data = z.infer<typeof dataSchema>;
