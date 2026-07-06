5b7141c441dff94f2d3691b43d899e0e4bce5d8f
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2026-07-06 15:10:30 +0200

    feat: add hooks to translate resource kinds

--- src/models/types/resource-kind.ts
+++ src/models/types/resource-kind.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { createTypeTranslationHooks } from "./_base";
 
 //------------------------------------------------------------------------------
 // Resource Kind
@@ -6,6 +7,7 @@ import { z } from "zod";
 
 export const resourceKindSchema = z.enum([
   "armor",
+  "armor_modifier",
   "background",
   "character_class",
   "character_subclass",
@@ -14,13 +16,10 @@ export const resourceKindSchema = z.enum([
   "eldritch_invocation",
   "equipment",
   "equipment_modifier",
-  "armor_modifier",
-  "item_modifier",
-  "tool_modifier",
-  "weapon_modifier",
   "feat",
   "feature",
   "item",
+  "item_modifier",
   "language",
   "maneuver",
   "metamagic",
@@ -31,10 +30,54 @@ export const resourceKindSchema = z.enum([
   "species",
   "spell",
   "tool",
+  "tool_modifier",
   "vehicle",
   "weapon",
+  "weapon_modifier",
 ]);
 
 export const resourceKinds = resourceKindSchema.options;
 
 export type ResourceKind = z.infer<typeof resourceKindSchema>;
+
+//------------------------------------------------------------------------------
+// Resource Visibility Hooks
+//------------------------------------------------------------------------------
+
+export const {
+  useOptions: useResourceKindOptions,
+  useTranslate: useTranslateResourceKind,
+  useTranslations: useResourceKindTranslations,
+} = createTypeTranslationHooks(resourceKinds, {
+  armor: { en: "Armor", it: "Armatura" },
+  armor_modifier: { en: "Armor Modifier", it: "Modificatore Armatura" },
+  background: { en: "Background", it: "Background" },
+  character_class: { en: "Class", it: "Classe" },
+  character_subclass: { en: "Subclass", it: "Sottoclasse" },
+  creature: { en: "Creature", it: "Creatura" },
+  creature_tag: { en: "Creature Tag", it: "Tag Creatura" },
+  eldritch_invocation: { en: "Eldritch Invocation", it: "Supplica Occulta" },
+  equipment: { en: "Equipment", it: "Equipaggiamento" },
+  equipment_modifier: {
+    en: "Equipment Modifier",
+    it: "Modificatore Equipaggiamento",
+  },
+  feat: { en: "Feat", it: "Talento" },
+  feature: { en: "Feature", it: "Privilegio" },
+  item: { en: "Item", it: "Oggetto" },
+  item_modifier: { en: "Item Modifier", it: "Modificatore Oggetto" },
+  language: { en: "Language", it: "Linguaggio" },
+  maneuver: { en: "Maneuver", it: "Manovra" },
+  metamagic: { en: "Metamagic", it: "Metamagia" },
+  modifier: { en: "Modifier", it: "Modificatore" },
+  plane: { en: "Plane", it: "Piano" },
+  resource: { en: "Resource", it: "Risorsa" },
+  service: { en: "Service", it: "Servizio" },
+  species: { en: "Species", it: "Specie" },
+  spell: { en: "Spell", it: "Incantesimo" },
+  tool: { en: "Tool", it: "Strumento" },
+  tool_modifier: { en: "Tool Modifier", it: "Modificatore Strumento" },
+  vehicle: { en: "Vehicle", it: "Veicolo" },
+  weapon: { en: "Weapon", it: "Arma" },
+  weapon_modifier: { en: "Weapon Modifier", it: "Modificatore Arma" },
+});
