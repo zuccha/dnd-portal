def7d79a5721eb394b48d711fb72c91f0ee0a247
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2026-09-17 10:15:54 +0200

    refactor: split localization context from localize functions for species

--- src/models/resources/species/localized-species.ts
+++ src/models/resources/species/localized-species.ts
@@ -1,6 +1,5 @@
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
 import z from "zod";
-import { useI18nLangContext } from "~/i18n/i18n-lang-context";
 import { translate } from "~/i18n/i18n-string";
 import { useI18nSystem } from "~/i18n/i18n-system";
 import { useFormatCmWithUnit } from "~/measures/distance";
@@ -8,10 +7,12 @@ import { useFormatFeatureEntries } from "../../other/feature-entries";
 import { useTranslateCreatureSize } from "../../types/creature-size";
 import { useTranslateCreatureType } from "../../types/creature-type";
 import {
+  type ResourceLocalizationContext,
   formatDetails,
   formatInfo,
+  localizeResource,
   localizedResourceSchema,
-  useLocalizeResource,
+  useResourceLocalizationContext,
 } from "../localized-resource";
 import { type Species, speciesSchema } from "./species";
 
@@ -33,50 +34,72 @@ export const localizedSpeciesSchema = localizedResourceSchema(
 export type LocalizedSpecies = z.infer<typeof localizedSpeciesSchema>;
 
 //------------------------------------------------------------------------------
-// Use Localized Species
+// Species Localization Context
 //------------------------------------------------------------------------------
 
-export function useLocalizeSpecies(sourceId: string): (species: Species) => LocalizedSpecies {
-  const localizeResource = useLocalizeResource<Species>();
-  const { lang, t, ti } = useI18nLangContext(i18nContext);
+type SpeciesLocalizationContext = ResourceLocalizationContext & {
+  formatCm: ReturnType<typeof useFormatCmWithUnit>;
+  formatFeatureEntries: ReturnType<typeof useFormatFeatureEntries>;
+  translateCreatureSize: ReturnType<typeof useTranslateCreatureSize>;
+  translateCreatureType: ReturnType<typeof useTranslateCreatureType>;
+};
 
-  const translateCreatureSize = useTranslateCreatureSize(lang);
-  const translateCreatureType = useTranslateCreatureType(lang);
-  const formatFeatureEntriesDetails = useFormatFeatureEntries(sourceId);
+//------------------------------------------------------------------------------
+// Use Species Localization Context
+//------------------------------------------------------------------------------
+
+function useSpeciesLocalizationContext(sourceId: string): SpeciesLocalizationContext {
+  const context = useResourceLocalizationContext(i18nContext);
+
+  const translateCreatureSize = useTranslateCreatureSize(context.lang);
+  const translateCreatureType = useTranslateCreatureType(context.lang);
+  const formatFeatureEntries = useFormatFeatureEntries(sourceId);
 
   const [system] = useI18nSystem();
   const formatCm = useFormatCmWithUnit(system === "metric" ? "m" : "ft");
 
-  return useCallback(
-    (species: Species): LocalizedSpecies => {
-      const description = translate(species.description, lang);
-      const features = formatFeatureEntriesDetails(species.feature_entries);
-      const sizes = species.sizes.map((size) => translateCreatureSize(size)).join("/");
-      const type = translateCreatureType(species.type);
-      const speed = formatCm(species.speed);
-
-      return {
-        ...localizeResource(species),
-        descriptor: ti("descriptor", sizes, type),
-
-        details: formatDetails(description, features),
-        info: formatInfo([[t("speed"), speed]]),
-        sizes,
-        speed,
-        type,
-      };
-    },
-    [
+  return useMemo(
+    () => ({
+      ...context,
       formatCm,
-      formatFeatureEntriesDetails,
-      lang,
-      localizeResource,
-      t,
-      ti,
+      formatFeatureEntries,
       translateCreatureSize,
       translateCreatureType,
-    ],
+    }),
+    [context, formatCm, formatFeatureEntries, translateCreatureSize, translateCreatureType],
   );
+}
+
+//------------------------------------------------------------------------------
+// Localize Species
+//------------------------------------------------------------------------------
+
+function localizeSpecies(species: Species, context: SpeciesLocalizationContext): LocalizedSpecies {
+  const description = translate(species.description, context.lang);
+  const features = context.formatFeatureEntries(species.feature_entries);
+  const sizes = species.sizes.map(context.translateCreatureSize).join("/");
+  const type = context.translateCreatureType(species.type);
+  const speed = context.formatCm(species.speed);
+
+  return {
+    ...localizeResource(species, context),
+    descriptor: context.ti("descriptor", sizes, type),
+
+    details: formatDetails(description, features),
+    info: formatInfo([[context.t("speed"), speed]]),
+    sizes,
+    speed,
+    type,
+  };
+}
+
+//------------------------------------------------------------------------------
+// Use Localize Species
+//------------------------------------------------------------------------------
+
+export function useLocalizeSpecies(sourceId: string): (species: Species) => LocalizedSpecies {
+  const context = useSpeciesLocalizationContext(sourceId);
+  return useCallback((species) => localizeSpecies(species, context), [context]);
 }
 
 //------------------------------------------------------------------------------
